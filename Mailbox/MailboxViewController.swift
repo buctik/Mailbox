992d01a89306ea6f6e549f5f114ffc15062ff23f
Omar Siddiqui <osiddiq@L-SB8D023G8W-M.local>
2016-06-05 21:41:15 -0700

    pans left with gray, right with green
    made lots of structural fixes. got rid of two image views which were
    subviews

--- Mailbox/MailboxViewController.swift
+++ Mailbox/MailboxViewController.swift
@@ -10,14 +10,41 @@ import UIKit
 
 class MailboxViewController: UIViewController, UIScrollViewDelegate {
 
+    
+    //Scroll View outlet
     @IBOutlet weak var scrollView: UIScrollView!
     
+    
+    //Scroll View Subview, ImageView outlets
     @IBOutlet weak var helpLabelImageView: UIImageView!
     @IBOutlet weak var searchImageView: UIImageView!
     @IBOutlet weak var feedImageView: UIImageView!
     
-    @IBOutlet weak var singleMessageView: UIImageView!
+    //MessageView container outlet
+    @IBOutlet weak var singleMessageView: UIView!
+
+    //messageImage outlet
+    @IBOutlet weak var messageImageView: UIImageView!
+    
+    // pan left imageView Outlets
+    @IBOutlet weak var laterImageView: UIImageView!
+    @IBOutlet weak var listImageView: UIImageView!
+    
+    // pan right imageView Outlets
+    @IBOutlet weak var archiveImageView: UIImageView!
+    @IBOutlet weak var deleteImageView: UIImageView!
+    
+    
+
+    
     var messageViewOriginalCenter: CGPoint!
+    
+    var laterViewOriginalCenter: CGPoint!
+    var listViewOriginalCenter: CGPoint!
+    
+    var archiveViewOriginalCenter: CGPoint!
+    var deleteViewOriginalCenter: CGPoint!
+    
     
     
     override func viewDidLoad() {
@@ -29,25 +56,76 @@ class MailboxViewController: UIViewController, UIScrollViewDelegate {
         
         // Do any additional setup after loading the view.
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
     @IBAction func didPanMessage(sender: UIPanGestureRecognizer) {
         
+        let location = sender.locationInView(view)
         let translation = sender.translationInView(view)
+        let velocity = sender.velocityInView(view)
+    
         
         
         if sender.state == UIGestureRecognizerState.Began {
             print("Gesture began")
-        messageViewOriginalCenter = singleMessageView.center
+        messageViewOriginalCenter = messageImageView.center
+            
+        
+            
             
         } else if sender.state == UIGestureRecognizerState.Changed {
-            singleMessageView.center = CGPoint(x: self.messageViewOriginalCenter.x + translation.x, y: self.messageViewOriginalCenter.y)
             
-            print("Gesture is changing")
+            if translation.x > 0  {
+                
+                messageImageView.center = CGPoint(x: self.messageViewOriginalCenter.x + translation.x, y: self.messageViewOriginalCenter.y)
+                
+                
+                singleMessageView.backgroundColor = UIColor.greenColor()
+             
+                
+                
+                /*
+            } else if translation.x > 259 && translation.x < 260{
+                singleMessageView.center = CGPoint(x: self.messageViewOriginalCenter.x + translation.x, y: self.messageViewOriginalCenter.y)
+                
+                
+                archiveView.backgroundColor = UIColor.yellowColor()
+                archiveView.frame.origin = CGPoint(x:singleMessageView.frame.width + translation.x, y: self.messageViewOriginalCenter.y)
+                */
+            } else if translation.x < 0 {
+                
+                messageImageView.center = CGPoint(x: self.messageViewOriginalCenter.x + translation.x, y: self.messageViewOriginalCenter.y)
+                
+                singleMessageView.backgroundColor = UIColor.grayColor()
+             
+                
+                
+                /*
+                 archiveView.backgroundColor = UIColor.grayColor()
+                 archiveView.frame.origin = CGPoint(x:singleMessageView.frame.width + translation.x, y: self.messageViewOriginalCenter.y)
+ 
+                
+                
+            } else if translation.x > -60 && translation.x < -260{
+                singleMessageView.center = CGPoint(x: self.messageViewOriginalCenter.x + translation.x, y: self.messageViewOriginalCenter.y)
+                
+                /*
+                 archiveView.backgroundColor = UIColor.yellowColor()
+                 archiveView.frame.origin = CGPoint(x:singleMessageView.frame.width + translation.x, y: self.messageViewOriginalCenter.y)
+                 */  */
+            }
+            
+            
+            
+            print("\(translation)")
         } else if sender.state == UIGestureRecognizerState.Ended {
             
             print("Gesture ended")
